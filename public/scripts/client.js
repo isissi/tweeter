c90919843ae7d1dbc9a4c9a0dd73bfdd8dba5e54
isissi <isissi_525@hotmail.com>
2021-12-17 13:23:21 -0700

    add slideup & slidedown

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,33 +3,16 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 
+//Preventing XSS with Escaping
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
 const creatTweetElement = function (tweetData) {
+  const safeTweet = `<p id="tweet">${escape(tweetData.content.text)}</p>`
   const html = `
   <article> 
     <header class="tweet-header">
@@ -40,14 +23,12 @@ const creatTweetElement = function (tweetData) {
       <span id="user-name">${tweetData.user.handle}</span>
     </header>
 
-    <p id="tweet">
-      ${tweetData.content.text}
-    </p>
+    ${safeTweet}
 
     <footer>
       <hr>
       <div class="date-icon-group">
-        <span id="date">${createdDate(tweetData)}</span>
+        <span id="date">${timeago.format(tweetData.created_at)}</span>
           <div>
             <i class="fas fa-flag fa-xs icons"></i>
             <i class="fas fa-retweet fa-xs icons"></i>
@@ -62,45 +43,7 @@ const creatTweetElement = function (tweetData) {
 }
 
 
-// Generate create time
-const createdDate = function (tweetData) {
-  const today = new Date();
-  const date = new Date(parseInt(tweetData.created_at));
-  const timeAgo = (today - date) / 1000;
-
-  if (timeAgo <= 60) {
-    return `Just posted`;
-  } else if (timeAgo > 60 && timeAgo < 3600) {
-    result = Math.floor(timeAgo / 60);
-    return `${result} minutes ago`;
-  } else if (timeAgo > 360 && timeAgo < 84600) {
-    result = Math.floor(timeAgo / 60 / 60);
-    if (result === 1) {
-      return `1 hour ago`;
-    }
-    return `${result} hours ago`
-  } else if (timeAgo >= 84600 &&  timeAgo < 2592000) {
-    result = Math.floor(timeAgo / 60 / 60 / 24);
-    if (result === 1) {
-      return `1 day ago`;
-    }
-    return `${result} days ago`;
-  } else if (timeAgo > 2592000 && timeAgo < 31536000) {
-    result = Math.floor(timeAgo / 60 / 60 / 24 / 30);
-    if (result === 1) {
-      return `1 month ago`;
-    }
-    return `${result} months ago`;
-  } else {
-    result = Math.floor(timeAgo / 31536000);
-    if (result === 1) {
-      return `1 year ago`;
-    }
-    return `${result} years ago`;
-  }
-
-}
-
+//Loop through all tweets and append to index.html
 const renderTweets = function (tweets) {
   for (const tweet of tweets) {
     const newTweet = creatTweetElement(tweet);
@@ -110,28 +53,66 @@ const renderTweets = function (tweets) {
   }
 }
 
+//responsible for fetching tweets from the http://localhost:8080/tweets page
+const loadTweets = function () {
+  $.ajax('/tweets', { method: 'GET' })
+  .then((res) => {
+    renderTweets(res);
+  });
+}
+
+loadTweets();
+
 
 $(document).ready(function () {
-  //Render past tweets
-  renderTweets(data);
-
   //Add an Event Listener and Prevent the Default Behaviour
   $("form").submit(function (event) {
     event.preventDefault();
 
-    const form = $(this);
-
-    $.ajax({
-      type: "POST",
-      url:"/tweets", 
-      data: form.serialize()
-    }).then(() => {
-      return $.ajax('/tweets/', { method: 'GET' });
-    }).then((res) => {
-
-      renderTweets(res);
-      form[0].reset();
-      form.find(".counter").text(140);
+    //When submit slide up all errors and remove the error element
+    $('.error').slideUp("slow", () => {
+      $('.error').remove();
     });
-})
+
+    const form = $(this);
+    const input = form.find("#tweet-text").val();
+    
+    //Add error message
+    if (!input) {
+      $(".new-tweet").prepend(
+        `<div class="error empty-sbmt">
+          <i class="far fa-dizzy"></i>
+          <p><b>Error: </b>Sorry, you can't publish a empty tweet. </p>
+        </div>`);
+      $(".empty-sbmt").slideDown("slow");
+     
+    } else if (input.length > 140) {
+      $(".new-tweet").prepend(
+        `<div class="error exceed-sbmt">
+          <i class="far fa-dizzy"></i>
+          <p><b>Error: </b>Sorry, the tweet you are trying to publish exceeds maximum character limit. </p>
+        </div>`);
+      $(".exceed-sbmt").slideDown("slow");
+    } else {
+
+      $.ajax({
+        type: "POST",
+        url: "tweets",
+        data: form.serialize(),
+      })
+
+      .then(() => {
+        return $.ajax('/tweets', { method: 'GET' });
+        })
+
+      .then((res) => {
+        //Reset form input and counter
+        renderTweets(res);
+        form[0].reset();
+        form.find(".counter").text(140);
+      })
+    }
+
+  })
 });
+
